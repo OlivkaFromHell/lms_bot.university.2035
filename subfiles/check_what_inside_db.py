735ec4c7273ec6917063fc7188b2994a7006368d
OlivkaFromHell <36050417+OlivkaFromHell@users.noreply.github.com>
2021-07-12 13:10:02 +0300

    Delete subfiles directory

--- subfiles/check_what_inside_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("data/user_database.db")
-cursor = conn.cursor()
-
-sql = "SELECT moodle_id FROM user_db WHERE email = 'vadim.tvj@gmail.com'"
-
-cursor.execute(sql)
-print(cursor.fetchone()[0])
